0871e62f12c8e00ba621a5b46bfb3c933fd4ff59
datomo <dave.lengw@gmail.com>
2023-03-10 15:18:18 +0100

    removing redundant values from entities

--- plugins/sql-language/src/main/java/org/polypheny/db/sql/sql2alg/SqlToAlgConverter.java
+++ plugins/sql-language/src/main/java/org/polypheny/db/sql/sql2alg/SqlToAlgConverter.java
@@ -2105,8 +2105,8 @@ public class SqlToAlgConverter implements NodeToAlgConverter {
         // Expand table macro if possible. It's more efficient than LogicalTableFunctionScan.
         final SqlCallBinding callBinding = new SqlCallBinding( bb.scope.getValidator(), bb.scope, call );
         if ( operator instanceof SqlUserDefinedTableMacro udf ) {
-            //final TranslatableEntity table = udf.getLogicalTable( typeFactory, callBinding.sqlOperands() );
-            //final LogicalTable catalogTable = Catalog.getInstance().getLogicalTable( table.getId() );
+            //final TranslatableEntity table = udf.getTable( typeFactory, callBinding.sqlOperands() );
+            //final LogicalTable catalogTable = Catalog.getInstance().getTable( table.getId() );
             //final AlgDataType rowType = table.getRowType( typeFactory );
             //AlgOptEntity algOptEntity = AlgOptEntityImpl.create( null, rowType, table, catalogTable, null );
             //AlgNode converted = toAlg( algOptEntity );
