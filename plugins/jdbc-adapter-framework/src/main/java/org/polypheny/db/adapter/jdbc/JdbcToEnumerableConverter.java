f07c326de8b75bb89e1063a74fdee0b63b3e5783
Tobias Hafner <mail@tobiashafner.ch>
2023-06-20 01:55:17 +0200

    Revert "Merge remote-tracking branch 'origin/proto-interface' into proto-interface"
    This reverts commit 68e35aae209a7423d1f5720b36842befb6057a25, reversing
    changes made to 960a099ca247c024989401a87397f6acd7347e42.

--- plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcToEnumerableConverter.java
+++ plugins/jdbc-adapter-framework/src/main/java/org/polypheny/db/adapter/jdbc/JdbcToEnumerableConverter.java
@@ -39,8 +39,6 @@ import java.lang.reflect.Modifier;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
